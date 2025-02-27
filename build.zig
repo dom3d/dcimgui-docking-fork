91460bba312a23d1f87ed60be5ba6fc1e8e4ffeb
Dominique Boutin <dominique@digitgaming.com>
2025-02-27 12:02:04 +0000

    cimgui header translation included

--- build.zig
+++ build.zig
@@ -7,6 +7,28 @@ const builtin = @import("builtin");
 pub fn build(b: *std.Build) void {
     const opt_with_docking = b.option(bool, "with_docking", "Uses the docking branch of (c)ImGui") orelse false;
 
+    // these are the files to use for dockin enabled
+    const docking_cpp_files = &.{
+        "src-docking/cimgui.cpp",
+        "src-docking/imgui_demo.cpp",
+        "src-docking/imgui_draw.cpp",
+        "src-docking/imgui_tables.cpp",
+        "src-docking/imgui_widgets.cpp",
+        "src-docking/imgui.cpp",
+    };
+    const docking_h_file = "src-docking/cimgui.h";
+
+    // these are the files to use for no docking
+    const non_docking_cpp_files = &.{
+        "src/cimgui.cpp",
+        "src/imgui_demo.cpp",
+        "src/imgui_draw.cpp",
+        "src/imgui_tables.cpp",
+        "src/imgui_widgets.cpp",
+        "src/imgui.cpp",
+    };
+    const non_docking_h_file = "src/cimgui.h";
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -18,29 +40,10 @@ pub fn build(b: *std.Build) void {
     });
     lib_cimgui.linkLibCpp();
 
-    if (opt_with_docking) {
-        lib_cimgui.addCSourceFiles(.{
-            .files = &.{
-                "src-docking/cimgui.cpp",
-                "src-docking/imgui_demo.cpp",
-                "src-docking/imgui_draw.cpp",
-                "src-docking/imgui_tables.cpp",
-                "src-docking/imgui_widgets.cpp",
-                "src-docking/imgui.cpp",
-            },
-        });
-    } else {
-        lib_cimgui.addCSourceFiles(.{
-            .files = &.{
-                "src/cimgui.cpp",
-                "src/imgui_demo.cpp",
-                "src/imgui_draw.cpp",
-                "src/imgui_tables.cpp",
-                "src/imgui_widgets.cpp",
-                "src/imgui.cpp",
-            },
-        });
-    }
+    lib_cimgui.addCSourceFiles(.{
+        .files = if (opt_with_docking) docking_cpp_files else non_docking_cpp_files,
+    });
+
     // make cimgui available as artifact, this allows to inject
     // the Emscripten sysroot include path in another build.zig
     b.installArtifact(lib_cimgui);
@@ -49,7 +52,7 @@ pub fn build(b: *std.Build) void {
     // NOTE: running this step with the host target is intended to avoid
     // any Emscripten header search path shenanigans
     const translateC = b.addTranslateC(.{
-        .root_source_file = b.path("src/cimgui.h"),
+        .root_source_file = b.path(if (opt_with_docking) docking_h_file else non_docking_h_file),
         .target = b.graph.host,
         .optimize = optimize,
     });
